57779779c93fdc6594ee7fa89309c770ae34c8ea
Ingo Fahrentholz <ingof.work@gmail.com>
2019-01-09 01:25:56 +0100

    testing somemore

--- index.js
+++ index.js
@@ -7,23 +7,6 @@ const filename = `about.txt`;
 
 fs.writeFileSync(filename, fileContent);
 
-simpleGit.mergeFromTo("aboutPage", "master", null, () => {
-  simpleGit.checkout("master", () => {
-    simpleGit.add(".", () => {
-      simpleGit.commit("added about page", null, null, () => {
-        simpleGit.push("origin", "master", () => {
-          console.log("DONE");
-          simpleGit.raw("git branch -D aboutPage", () => {
-            console.log("removed local branch aboutPage");
-            simpleGit.raw("git push origin --delete aboutPage", () => {
-              console.log("removed remote branch aboutPage");
-            });
-          });
-        });
-      });
-    });
-  });
-});
 // simpleGit
 //   // .checkoutBranch("aboutPage", "master")
 //   // .add(filename)
@@ -33,3 +16,23 @@ simpleGit.mergeFromTo("aboutPage", "master", null, () => {
 //   .add()
 //   .commit("added aboutPage")
 //   .push("origin", "master");
+
+// simpleGit.mergeFromTo("aboutPage", "master", null, () => {
+//   simpleGit.checkout("master", () => {
+//     simpleGit.add(".", () => {
+//       simpleGit.commit("added about page", null, null, () => {
+//         simpleGit.push("origin", "master", () => {
+//           console.log("DONE");
+//           simpleGit.raw("git branch -D aboutPage", () => {
+//             console.log("removed local branch aboutPage");
+//             simpleGit.raw("git push origin --delete aboutPage", () => {
+//               console.log("removed remote branch aboutPage");
+//             });
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
+
+// simpleGit.raw(["push", "origin", "--delete", "bingo"]);
