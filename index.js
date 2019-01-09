9d2c0a962cdd31c9b65633b2edde3d1acf430e9b
Ingo Fahrentholz <ingof.work@gmail.com>
2019-01-09 01:09:12 +0100

    added about page

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ var fs = require("fs");
 const uuidv1 = require("uuid/v1");
 const simpleGit = require("simple-git")(__dirname);
 
-const fileContent = "Hello Ingo";
-const filename = `${uuidv1()}.txt`;
+const fileContent = "About Page";
+const filename = `about.txt`;
 
 fs.writeFileSync(filename, fileContent);
 
-simpleGit
-  .add(filename)
-  .commit(`Add ${filename}`)
-  .push("origin", "master");
+simpleGit.mergeFromTo("aboutPage", "master", null, () => {
+  simpleGit.checkout("master", () => {
+    simpleGit.add(".", () => {
+      simpleGit.commit("added about page", null, null, () => {
+        simpleGit.push("origin", "master", () => {
+          console.log("DONE");
+        });
+      });
+    });
+  });
+});
+// simpleGit
+//   // .checkoutBranch("aboutPage", "master")
+//   // .add(filename)
+//   // .commit(`Add ${filename}`)
+//   .mergeFromTo("aboutPage", "master")
+//   .checkout("master")
+//   .add()
+//   .commit("added aboutPage")
+//   .push("origin", "master");
